5bceb84db9c61889bb9fef003072bbc0fbe6dae0
Pradyumn Agrawal <geniuspradyumn@gmail.com>
2016-03-19 10:29:57 +0530

    Update Time Conversion.c

--- warmup/Time Conversion.c
+++ warmup/Time Conversion.c
@@ -1,3 +1,9 @@
+/*
+Given a time in AM/PM format, convert it to military (24-hour) time.
+Note: Midnight is 12:00:00 AM on a 12-hour clock and 00:00:00 on a 24-hour clock. Noon is 12:00:00 PM on a 12-hour clock and 
+12:00:00 on a 24-hour clock. 
+*/
+
 #include<stdio.h>
 #include<math.h>
 #include<string.h>
