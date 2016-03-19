18fc7fb6895fa6482e2f9ee11279dec2aff7bb9c
Pradyumn Agrawal <geniuspradyumn@gmail.com>
2016-03-19 10:22:27 +0530

    Update A Very Big Sum.c

--- warmup/A Very Big Sum.c
+++ warmup/A Very Big Sum.c
@@ -1,4 +1,5 @@
-/*You are given an array of integers of size N. You need to print the sum of the elements in the array, keeping in mind that some of those integers may be quite large.*/
+/*You are given an array of integers of size N. You need to print the sum of the elements in the array, 
+keeping in mind that some of those integers may be quite large.*/
 
 #include <stdio.h>
 #include <string.h>
